3ce8a7e76914ef9032b97221e5c5dd133b7dc456
NorhakimYusof <norhakim82@gmail.com>
2014-10-12 12:11:30 +0200

    changes

--- R_scatterPlot.py
+++ R_scatterPlot.py
@@ -9,21 +9,10 @@ from rpy2.robjects.packages import importr
 # The R 'print' function
 rprint = robjects.globalenv.get("print")
 lattice = importr('lattice')
-##datasets = importr('datasets')
-
-### Plot
-##xyplot = lattice.xyplot
 
 # Dataset
 mtcars = datasets.__rdata__.fetch('mtcars')['mtcars']
 
-##formula = Formula('mpg ~ wt')
-##formula.getenvironment()['mpg'] = mtcars.rx2('mpg')
-##formula.getenvironment()['wt'] = mtcars.rx2('wt')
-##
-##p = lattice.xyplot(formula)
-##
-##rprint(p)
 
 # Plot
 gp = ggplot2.ggplot(mtcars)
